fc01aaa8c5ec8a2ab53872b0d956fad469ef9293
DimitryRukhadze <lokiinflames@gmail.com>
2022-08-27 17:36:07 +0300

    Fixed Fn doubles

--- places/admin.py
+++ places/admin.py
@@ -32,16 +32,5 @@ class PlacesAdmin(SortableAdminBase, admin.ModelAdmin):
 
 
 @admin.register(Image)
-class ImagesAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ['img_file', 'place', 'queue_position']
-    readonly_fields = ['img_preview']
-    ordering = ['queue_position']
-
-    def img_preview(self, obj):
-        height = obj.img_file.height
-        width = obj.img_file.width
-        while height > 200:
-            height /= 2
-            width /= 2
-
-        return format_html(f'<img src="{obj.img_file.url}" width="{width/2}" height={height/2} />')
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['img_file', 'place']
